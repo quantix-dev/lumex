588e1c36e609eae6fc7ee892ce4747bdfe38c782
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2024-06-13 22:31:11 +0100

    feat(config): add type support for runtime configuration

--- examples/simple-bot/lumina.config.ts
+++ examples/simple-bot/lumina.config.ts
@@ -1,6 +1,12 @@
-import { defineBotConfig } from 'lumina/config'
+import process from 'node:process'
+import { defineLuminaConfig } from 'lumina/config'
 
-export default defineBotConfig({
+export default defineLuminaConfig({
   name: 'simple-bot',
   intents: [],
+  runtime: {
+    botToken: process.env.LUMINA_BOT_TOKEN,
+    devToken: process.env.LUMINA_DEV_TOKEN,
+    devGuild: process.env.LUMINA_DEV_GUILD,
+  },
 })
--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -1,6 +1,45 @@
-import type { BitFieldResolvable, GatewayIntentsString } from 'discord.js'
+import type { ClientOptions } from 'discord.js'
 
-interface Config {
+type UpperSnakeCase<T extends string> = Uppercase<T>
+
+const hint = Symbol('hint')
+type EnvValue<T, Path extends string, K extends string> = T & { [hint]?: `You can override this value with LUMINA${Path}_${UpperSnakeCase<K>}` }
+type EnvConfig<T extends Record<string, any>, Path extends string = ''> = {
+  [K in keyof T]?: K extends string
+    ? unknown extends T[K]
+      ? unknown
+      : T[K] extends Record<string, unknown>
+        ? EnvValue<EnvConfig<T[K], `${Path}_${UpperSnakeCase<K>}`>, Path, K>
+        : EnvValue<T[K], Path, K>
+    : K extends number
+      ? T[K]
+      : never
+}
+
+export interface RuntimeConfig {
+  /**
+   * The production bot token.
+   */
+  botToken: string
+
+  /**
+   * The development bot token.
+   * Uses the {@link RuntimeConfig.botToken | botToken} if a value is not provided.
+   */
+  devToken?: string
+
+  /**
+   * The development guild.
+   */
+  devGuild?: string
+
+  /**
+   * The public values accessible from interactions, plugins, etc.
+   */
+  public?: Record<string, any>
+}
+
+export interface LuminaConfig {
   /**
    * The name of the bot.
    */
@@ -8,10 +47,19 @@ interface Config {
 
   /**
    * Discord intents to enable for this connection.
+   * @see {@link https://discord.com/developers/docs/topics/gateway#list-of-intents}
    */
-  intents: BitFieldResolvable<GatewayIntentsString, number>
+  intents: ClientOptions['intents']
+
+  /**
+   * Runtime bot configuration.
+   */
+  runtime: EnvConfig<RuntimeConfig>
 }
 
-export function defineBotConfig(_config: Config) {
-  return _config
+/**
+ * Define Lumina config.
+ */
+export function defineLuminaConfig(config: LuminaConfig) {
+  return config
 }
